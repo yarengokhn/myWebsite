0a5b39035f2a85d96ee6a5db16dd3021065e5d5a
yaren <yarengokhn@gmail.com>
2024-11-12 21:33:43 +0300

    Product modeli eklendi ve admin tarafinda gösterildi.

--- myProject/product/models.py
+++ myProject/product/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -13,7 +14,8 @@ class Category(models.Model):
     slug = models.SlugField(null=False, unique=True)
     parent = models.ForeignKey('self', blank=True, null=True,
                                related_name='children', on_delete=models.CASCADE)
-    create_at = models.DateTimeField(auto_now=True)
+   
+    create_at=models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     discount_tag = models.CharField(max_length=30, blank =True, null = True)
@@ -23,3 +25,26 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+
+class Product(models.Model):
+    STATUS=( ('True','Evet'),('False','Hayir'))
+
+    productCategory =models.ForeignKey(Category,on_delete=models.CASCADE) #her ürün bir kategoriye ait olacak.
+    #Product modelini Category modeline bağlar ve bir kategori silindiğinde o kategoriye ait tüm ürünlerin de silinmesini sağlar.
+
+    user =models.ForeignKey(User,on_delete = models.CASCADE)
+    title=models.CharField(max_length=75)
+    keywords=models.CharField(blank=True,max_length=300)
+    description=models.CharField(blank=True,max_length=300)
+    image=models.ImageField(blank=True,upload_to='images/')
+    price=models.FloatField()
+    amount=models.IntegerField(default=0)
+    detail=models.TextField()
+    status=models.CharField(max_length=10,choices=STATUS,default='False')
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    slug=models.SlugField(null=False,unique=True)
+
+
+    def __str__(self):
+        return self.title
