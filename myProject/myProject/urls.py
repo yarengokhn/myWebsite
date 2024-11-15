c17daae8fcad2cdb746973d3074345a66dc37677
yaren <yarengokhn@gmail.com>
2024-11-15 18:13:17 +0300

    User app  olusturuldu.

--- myProject/myProject/urls.py
+++ myProject/myProject/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('home/', include('home.urls')),
     path('product/', include('product.urls')),
     path('order/', include('order.urls')),
+    path('user/', include('user.urls')),
     path('',homeviews.index,name = 'index'),#name='index': URL’e kolayca referans vermek için bir isim tanımlar.
     path('iletisim', homeviews.iletisim, name='iletisim'),
     path('aboutus', homeviews.aboutus, name='aboutus'),
---
+++ myProject/user/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from user import views
+
+urlpatterns = [
+    # path('', views.user_profile, name='user_profile'),
+    # path('login', views.user_login, name='user_login'),
+    # path('logout', views.user_logout, name='user_logout'),
+    # path('register', views.user_register, name='user_register'),
+    # path('update', views.user_update, name='user_update'),
+    # path('password', views.user_password, name='user_password'),
+    # path('mycomments', views.user_comments, name='user_comments'),
+    # path('deletecomment/<int:id>', views.user_delete_comment, name='user_delete_comment'),
+    # path('myorders', views.user_orders, name='user_orders'),
+    # path('myorders_products', views.user_order_products, name='user_order_products'),
+    # path('orderdetail/<int:id>', views.user_order_detail, name='user_order_detail'),
+    # path('orderdetail_product/<int:id>/<int:oid>', views.user_order_detail_product, name='user_order_detail_product'),
+    # path('myproducts/', views.user_products, name='user_products'),
+    # path('user_product_add', views.user_product_add, name='user_product_add'),
+    # path('delete_product/<int:id>', views.user_delete_product, name='user_delete_product'),
+    # path('update_product/<int:id>', views.user_update_product, name='user_update_product'),
+]
